410e7e8d37234ccfe4386f47375b12290892f4b4
welbadane1235-svg <wel.badane1235@gmail.com>
2026-04-23 09:43:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  supabaseUrl: 'ضع_رابط_سوبابيس_هنا',
-  supabaseAnonKey: 'ضع_المفتاح_هنا'
+  supabaseUrl: "https://zmjdqiswytxlbfgnfjfv.supabase.co",
+  supabaseAnonKey: "ضع هنا المفتاح الصحيح"
 };
